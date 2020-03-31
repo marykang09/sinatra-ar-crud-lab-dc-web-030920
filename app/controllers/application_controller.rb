d4b7fb2204904b302ac60b2042b19640e1f47482
dsuccar <danielsuccar@icloud.com>
2020-03-31 17:34:28 -0400

    finished the rread instructions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,40 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/' do #index
+    erb :index
   end
+
+  get '/articles/new' do #show form 
+    erb :new
+  end
+  
+  post '/articles' do #create article in database 
+    title = params[:title]
+    content = params[:content]
+    article = Article.create(title: title, content: content)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index  
+  end
+
+  get '/articles/:id' do
+ 
+    id = params[:id]
+    @article = Article.find(id)
+
+    erb :show
+  end
+
+  
+  get '/articles/:id/edit' do
+
+    erb :edit
+  end
+
+  
+
 end
